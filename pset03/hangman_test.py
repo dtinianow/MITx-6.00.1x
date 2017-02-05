64ba694455044df93e7b26ba501f62f960b0d877
David Tinianow <david.tinianow@gmail.com>
2017-02-05 15:05:05 -0700

    It can determine if word has been guessed

---
+++ pset03/hangman_test.py
@@ -0,0 +1,20 @@
+import unittest
+from hangman import *
+
+class HangmanTest(unittest.TestCase):
+    def test_it_can_determine_if_word_has_not_been_guessed(self):
+        secretWord = 'apple'
+        lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
+        actual = isWordGuessed(secretWord, lettersGuessed)
+        expected = False
+        self.assertEqual(actual, expected)
+
+    def test_it_can_determine_if_word_has_been_guessed(self):
+        secretWord = 'durian'
+        lettersGuessed = ['h', 'a', 'c', 'd', 'i', 'm', 'n', 'r', 't', 'u']
+        actual = isWordGuessed(secretWord, lettersGuessed)
+        expected = True
+        self.assertEqual(actual, expected)
+
+if __name__ == "__main__":
+    unittest.main()
