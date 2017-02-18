5c6f959ec18b57c0aa67662841afdb2d1e9d3e1e
David Tinianow <david.tinianow@gmail.com>
2017-02-17 17:52:44 -0700

    Complete is valid word function

--- pset04/ps4a.py
+++ pset04/ps4a.py
@@ -172,8 +172,18 @@ def isValidWord(word, hand, wordList):
     hand: dictionary (string -> int)
     wordList: list of lowercase strings
     """
-    # TO DO ... <-- Remove this comment when you code this function
+    current_hand = hand.copy()
 
+    if word not in wordList:
+        return False
+
+    for letter in word:
+        if current_hand.get(letter, 0) == 0:
+            return False
+
+        current_hand[letter] -= 1
+
+    return True
 
 #
 # Problem #4: Playing a hand
