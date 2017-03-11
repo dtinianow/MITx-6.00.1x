dbaca6d4c736e15958754a09303a6b1444f12b8e
David Tinianow <david.tinianow@gmail.com>
2017-03-10 22:11:58 -0700

    Add comment

--- final/p03/convert_to_mandarin.py
+++ final/p03/convert_to_mandarin.py
@@ -1,4 +1,8 @@
 def convert_to_mandarin(number):
+    '''
+    us_num, a string representing a US number 0 to 99
+    returns the string mandarin representation of us_num
+    '''
     trans = {'0':'ling', '1':'yi', '2':'er', '3':'san', '4': 'si',
              '5':'wu', '6':'liu', '7':'qi', '8':'ba', '9':'jiu', '10': 'shi'}
     mandarin = []
