1c7d59efeb0624494a89ffea988840cf8fa2f8d7
David Tinianow <david.tinianow@gmail.com>
2017-03-10 22:17:05 -0700

    Fix tests

--- final/p03/test_convert_to_mandarin.py
+++ final/p03/test_convert_to_mandarin.py
@@ -3,20 +3,20 @@ from convert_to_mandarin import *
 
 class ConvertToMandarinTest(unittest.TestCase):
     def test_it_can_convert_36_to_mandarin(self):
-        actual = 'san shi liu'
-        expected = convert_to_mandarin('36')
+        actual = convert_to_mandarin('36')
+        expected = 'san shi liu'
 
         self.assertEqual(actual, expected)
 
     def test_it_can_convert_20_to_mandarin(self):
-        actual = 'er shi'
-        expected = convert_to_mandarin('20')
+        actual = convert_to_mandarin('20')
+        expected = 'er shi'
 
         self.assertEqual(actual, expected)
 
     def test_it_can_convert_16_to_mandarin(self):
-        actual = 'shi liu'
-        expected = convert_to_mandarin('16')
+        actual = convert_to_mandarin('16')
+        expected = 'shi liu'
 
         self.assertEqual(actual, expected)
 
